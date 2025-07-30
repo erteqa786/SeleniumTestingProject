f6e7db11b57d8119132d27b1c9db711aa797a968
Erteqa Hossain <erteqahossain@gmail.com>
2025-07-30 17:16:22 +0530

    Initial commit of Selenium project

---
+++ src/main/java/com/demoaq/pages/forms/PracticeFormPage.java
@@ -0,0 +1,56 @@
+package com.demoaq.pages.forms;
+
+import org.openqa.selenium.By;
+
+import static utilities.JavaScriptUtility.clickJS;
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
+public class PracticeFormPage extends FormsPage{
+    private By femaleRadioButton= By.id("gender-radio-2");
+    private By sportsHobbyCheckbox= By.id("hobbies-checkbox-1");
+    private By readingHobbyCheckbox= By.id("hobbies-checkbox-2");
+    private By musicHobbyCheckbox= By.id("hobbies-checkbox-3");
+    public void clickFemaleRadioButton(){
+        scrollToElementJS(femaleRadioButton);
+        clickJS(femaleRadioButton);
+    }
+    public boolean isFemaleSelected(){
+        return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSportsHobbyCheckbox() {
+        if (!find(sportsHobbyCheckbox).isSelected()) {
+            scrollToElementJS(sportsHobbyCheckbox);
+            clickJS(sportsHobbyCheckbox);
+        }
+
+    }
+
+    public void clickReadingHobbyCheckbox() {
+        if (!find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+
+    }
+
+    public void clickMusicHobbyCheckbox() {
+        if (!find(musicHobbyCheckbox).isSelected()) {
+            scrollToElementJS(musicHobbyCheckbox);
+            clickJS(musicHobbyCheckbox);
+        }
+
+    }
+
+    public void unclickReadingHobbyCheckbox() {
+        if (find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+
+    }
+
+    public boolean isReadingSelected(){
+        return find(readingHobbyCheckbox).isSelected();
+    }
+}
---
+++ src/main/java/utilities/Utility.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import com.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class Utility {
+    public static WebDriver driver;
+
+    public static void setUtilityDriver(){
+        driver=BasePage.driver;
+    }
+    // Generate A random Number
+
+
+    //Return Strings to UpperCase
+}
